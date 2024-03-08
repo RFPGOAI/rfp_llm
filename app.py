b0df5f76135667c452ecf41d8f52ab3735310726
bpben <yksrotab@gmail.com>
2024-03-08 10:18:06 -0500

    refactor

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import json
-from langchain.llms import Ollama
 from langchain.vectorstores import FAISS
 from langchain.docstore.document import Document
 from langchain_community.embeddings import HuggingFaceEmbeddings
@@ -10,42 +9,59 @@ from langchain.llms import Ollama
 model_name = "sentence-transformers/all-MiniLM-L6-v2"
 
 # Create the LangChain embedding object
-embeddings = HuggingFaceEmbeddings(model_name=model_name)
+@st.cache_resource
+def get_embeddings(model_name):
+    return HuggingFaceEmbeddings(model_name=model_name)
+embeddings = get_embeddings(model_name)
 
-# ollama model
-llm = Ollama(model="gemma:7b")
-
-# doc store is all companies - metadata can be about where they came from
-simple = json.load(open('./rfp_data/company_simple.json'))
-cond = json.load(open('./rfp_data/company_conditions.json'))
-all = simple + cond
-companies = []
-for d in all:
-    text = d['response']['content']
-    doc =  Document(page_content=text, metadata=d['prompt'][0]) 
-    companies.append(doc)
+@st.cache_resource
+def get_doc_store():
+    # doc store is all companies - metadata can be about where they came from
+    simple = json.load(open('./rfp_data/company_simple.json'))
+    cond = json.load(open('./rfp_data/company_conditions.json'))
+    all = simple + cond
+    companies = []
+    for d in all:
+        text = d['response']['content']
+        doc =  Document(page_content=text, metadata=d['prompt'][0]) 
+        companies.append(doc)
+    return FAISS.from_documents(companies, embeddings)
 
 # vector store based on ollama embeddings of doc split
-db = FAISS.from_documents(companies, embeddings)
+db = get_doc_store()
 
+# specify source path
+fp = 'rfps_combinations_ext'
 # create metadata for rfps and save
 # don't need to run this multiple times
-# rfps = json.load(open('./rfp_data/rfps_conditions.json'))
+# ollama model
+# llm = Ollama(model="gemma:7b")
+# rfps = json.load(open(f'./rfp_data/{fp}.json'))
 
 # rfps_w_metadata = []
 # for r in rfps:
-#     prompt = f"""Extract the key considerations from this RFP as bullet points, \
-#         provide no additional information: {r['response']['content']}"""
+#     prompt = f"""Briefly describe the key considerations from this RFP as bullet points. \
+#         Do not preface your response: {r['response']['content']}"""
 #     key_considerations = llm.invoke(prompt)
 #     r['metadata'] = {'prompt': r['prompt'],
-#                      'key_considerations': key_considerations}
+#                     'key_considerations': key_considerations}
 #     rfps_w_metadata.append(r)
 
-# json.dump(rfps_w_metadata, open(f'./rfp_data/rfps_conditions_w_metadata.json', 'w'))
+# json.dump(rfps_w_metadata, open(f'./rfp_data/{fp}_w_metadata.json', 'w'))
 
 # Load the RFP data
-rfps = json.load(open('./rfp_data/rfps_conditions_w_metadata.json'))
+rfps = json.load(open(f'./rfp_data/{fp}_w_metadata.json'))
 
+# refresh workflow
+for r in rfps:
+    if 'metadata' not in r:
+        llm = Ollama(model="gemma:7b")
+        prompt = f"""Briefly describe the key considerations from this RFP as bullet points. \
+        Do not preface your response: {r['response']['content']}"""
+        key_considerations = llm.invoke(prompt)
+        r['metadata'] = {'prompt': r['prompt'],
+                    'key_considerations': key_considerations}
+        json.dump(rfps, open(f'./rfp_data/{fp}_w_metadata.json', 'w'))
 
 def get_sim_company(rfp_metadata):
     key_conds = rfp_metadata['key_considerations']
@@ -54,20 +70,47 @@ def get_sim_company(rfp_metadata):
 
 
 def main():
+    rfp_conditions = ['environmental sustainability',
+             'vendor is minority-owned',
+             'vendor is women-owned',
+             'vendor is local']
+
     st.sidebar.title('Select RFP prompt')
-    selected_rfp = st.sidebar.selectbox('Select prompt', 
-                         [p['prompt'].split(':')[1].strip('.')  
-                          for p in rfps])
-    selected_index = [p['prompt'].split(':')[1].strip('.') for p in rfps].index(selected_rfp)
+    checked_conds = {}
+    for i, c in enumerate(rfp_conditions):
+        if i == 0:
+            default=True
+        else:
+            default=False     
+        checked_conds[c] = st.sidebar.checkbox(c, value=default)
+    
+    condition_key = set([k for k, v in checked_conds.items() if v])
 
-    st.title('RFPs and Companies')
-    st.subheader('Selected RFP')
-    st.write(rfps[selected_index]['response']['content'])
+    selected_rfp = None
+    for r in rfps:
+        if set(r['condition']) == condition_key:
+            selected_rfp = r
+            break
 
-    st.subheader('Suggested company')
 
-    suggested_company = get_sim_company(rfps[selected_index]['metadata'])
-    st.write(suggested_company)
+    
+
+    st.title('RFP Analysis')
+    col1, col2, col3 = st.columns(3)
+    
+    if selected_rfp:
+        with col1:
+            st.subheader('Selected RFP')
+            st.write(selected_rfp['response']['content'])
+
+        with col2:
+            st.subheader('Key considerations')
+            st.write(selected_rfp['metadata']['key_considerations'].split('\n\n')[1])
+
+        with col3:
+            st.subheader('Suggested company')
+            suggested_company = get_sim_company(selected_rfp['metadata'])
+            st.write(suggested_company)
 
 if __name__ == "__main__":
     main()
